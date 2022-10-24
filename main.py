ae1faf913428a96708c99bf51807e699349ebaf1
ethanrasmussen <itseraz@gmail.com>
2022-10-24 17:21:59 -0500

    update

--- main.py
+++ main.py
@@ -30,3 +30,39 @@ def segment_by_deg(img, degInterval:int, widthPixels:int, center:tuple):
         segments.append(rotated_segment(img, deg, widthPixels, center))
         deg += degInterval
     return segments
+
+def cropped_segments(img, degInterval:int, widthPixels:int, center:tuple):
+    segments = segment_by_deg(img, degInterval, widthPixels, center)
+    new_pieces = []
+    for segment in segments:
+        new_pieces.append(segment[int(center[1] - (widthPixels / 2)):int(center[1] + (widthPixels / 2)), int(center[0]):int(center[0] + 999999999)])
+    return new_pieces
+
+from matplotlib import pyplot as plt
+def vertical_display(segments, cropped:bool, center=None, widthPixels=None):
+    if cropped:
+        for s in segments:
+            plt.figure()
+            plt.imshow(s)
+    else:
+        for segment in segments:
+            plt.figure()
+            plt.imshow(segment[int(center[1] - (widthPixels / 2)):int(center[1] + (widthPixels / 2)), int(center[0]):int(center[0] + 999999999)])
+
+# TODO: make this work properly
+def horizontal_display(segments, cropped:bool, center=None, widthPixels=None):
+    # adjust/rotate images
+    imgs = []
+    if cropped:
+        for segment in segments:
+            imgs.append(rotate_img(segment, 90))
+    else:
+        imgs = []
+        for segment in segments:
+            new = segment[int(center[1] - (widthPixels / 2)):int(center[1] + (widthPixels / 2)), int(center[0]):int(center[0] + 999999999)]
+            imgs.append(rotate_img(new, 90))
+    # build figure and display
+    plt.figure(figsize=(1, 2 * len(imgs)))
+    for i in range(len(imgs)):
+        plt.subplot(1, 2*len(imgs), i + 1)
+        plt.imshow(imgs[i])
