acd01f5c1cd4c6936d036ffb0237760f1786c146
ethanrasmussen <itseraz@gmail.com>
2022-11-06 21:42:48 -0600

    edits

---
+++ build_concatenated.py
@@ -0,0 +1,13 @@
+from main import *
+import matplotlib.image as mpimg
+
+# image = Image('data/ischemic/1_li.jpg')
+# plt.imshow(np.vstack(get_segments(img=image.img,degInterval=10,widthPixels=20,center=image.center)))
+# plt.savefig('test.jpg')
+#
+# mpimg.imsave('mp_test.jpg', np.vstack(get_segments(img=image.img,degInterval=10,widthPixels=20,center=image.center)))
+
+for i in range(1,21):
+    print(i)
+    image = Image(f'data/ischemic/{i}_li.jpg')
+    mpimg.imsave(f'{i}_li_concatenated.jpg', np.vstack(get_segments(img=image.img, degInterval=10, widthPixels=20, center=image.center)))
