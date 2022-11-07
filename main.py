8e6af4fd566470713add1f3cc14619ac9f09fb7d
ethanrasmussen <itseraz@gmail.com>
2022-11-06 21:23:47 -0600

    clean up codebase & prepare for adding healthy eye data

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 import pandas as pd
-from IPython.display import Image
 import cv2
 import numpy as np
 import imutils
+from matplotlib import pyplot as plt
 
-df = pd.read_excel('data/data.xlsx')
+# import pupil center data for images
+li_df = pd.read_excel('data/ischemic/data_li.xlsx')
+h_df = pd.read_excel('data/healthy/data_h.xlsx')
 
+####################################################
+### CLASS DEFINITIONS:
+
+# special class for holding cv2 images & their pupil center points in one object
 class Image:
     img = None
     center = None
     def __init__(self, filename:str):
-        index = int(filename.split('.jpg')[0].split('/')[1])
-        self.img = cv2.imread(filename, cv2.IMREAD_COLOR)
-        self.center = (df['centerX'][index], df['centerY'][index])
+        index = int(filename.split('.jpg')[0].split('/')[-1].replace('_h', '').replace('_li',''))
+        self.img = cv2.imread(filename, cv2.IMREAD_COLOR)[...,[2,1,0]]
+        if ('_li' in filename):
+            self.center = (li_df['centerX'][index], li_df['centerY'][index])
+        elif ('_h' in filename):
+            self.center = (h_df['centerX'][index], h_df['centerY'][index])
+        else:
+            self.center = None
 
+####################################################
+### HELPER FUNCTIONS:
+
+# returns image rotated by deg degrees
 def rotate_img(img_CV2, deg:int):
     return imutils.rotate(img_CV2, deg)
 
+# rotates image, then returns segment masked from given center (with defined width/Y value)
 def rotated_segment(img, deg:int, widthPixels:int, center:tuple):
     imgr = rotate_img(img, deg)
     mask = np.zeros(imgr.shape, dtype='uint8')
     cv2.rectangle(mask, (center[0], int(center[1] - (widthPixels / 2))), (center[0] + 25000000, int(center[1] + (widthPixels / 2))), (255,255,255), -1)
     return np.where(mask, imgr, np.zeros(imgr.shape, dtype='uint8'))
 
+# given turning/degree interval, returns multiple rotated uncropped segments encompassing entire image
 def segment_by_deg(img, degInterval:int, widthPixels:int, center:tuple):
     deg = 0
     segments = []
@@ -31,42 +48,8 @@ def segment_by_deg(img, degInterval:int, widthPixels:int, center:tuple):
         deg += degInterval
     return segments
 
-def cropped_segments(img, degInterval:int, widthPixels:int, center:tuple):
-    segments = segment_by_deg(img, degInterval, widthPixels, center)
-    new_pieces = []
-    for segment in segments:
-        new_pieces.append(segment[int(center[1] - (widthPixels / 2)):int(center[1] + (widthPixels / 2)), int(center[0]):int(center[0] + 999999999)])
-    return new_pieces
-
-from matplotlib import pyplot as plt
-def vertical_display(segments, cropped:bool, center=None, widthPixels=None):
-    if cropped:
-        for s in segments:
-            plt.figure()
-            plt.imshow(s)
-    else:
-        for segment in segments:
-            plt.figure()
-            plt.imshow(segment[int(center[1] - (widthPixels / 2)):int(center[1] + (widthPixels / 2)), int(center[0]):int(center[0] + 999999999)])
-
-# TODO: make this work properly
-def horizontal_display(segments, cropped:bool, center=None, widthPixels=None):
-    # adjust/rotate images
-    imgs = []
-    if cropped:
-        for segment in segments:
-            imgs.append(rotate_img(segment, 90))
-    else:
-        imgs = []
-        for segment in segments:
-            new = segment[int(center[1] - (widthPixels / 2)):int(center[1] + (widthPixels / 2)), int(center[0]):int(center[0] + 999999999)]
-            imgs.append(rotate_img(new, 90))
-    # build figure and display
-    plt.figure(figsize=(1, 2 * len(imgs)))
-    for i in range(len(imgs)):
-        plt.subplot(1, 2*len(imgs), i + 1)
-        plt.imshow(imgs[i])
-
+# TODO: fix math/function so it works properly
+# given cv2 image file and pupil center, crops image to make given center actual geometric center of image
 def recenter_img(img, center):
     x1,y1,x2,y2 = 0,0,0,0
     ## img_cv2 = img.img
@@ -88,6 +71,23 @@ def recenter_img(img, center):
     ## return img_cv2[x1:y1, x2:y2]
     return img[x1:y1, x2:y2]
 
+####################################################
+### RADIAL SEGMENTATION FUNCTIONS:
+
+# captures entire image in cropped segments
+def get_segments(img, degInterval:int, widthPixels:int, center:tuple):
+    segments = segment_by_deg(img, degInterval, widthPixels, center)
+    new_pieces = []
+    for segment in segments:
+        new_pieces.append(segment[int(center[1] - (widthPixels / 2)):int(center[1] + (widthPixels / 2)), int(center[0]):int(center[0] + 999999999)])
+    return new_pieces
+
+####################################################
+### EDGE DETECTION CODE:
+
+# TODO: tweak Canny edge detection or find alternative edge detection method that functions well
+# goal: detect limbus within individual segments
+
 def canny_edges(image_img):
     return cv2.Canny(image_img, 100, 200)
 
@@ -99,6 +99,42 @@ def show_canny(img_img):
     plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
     plt.show()
 
-# recenter img around pupil
-# color channel & concatenation
-# canny edge detection / binning
+
+####################################################
+### DEPRECATED CODE:
+
+# def get_segments(img, degInterval:int, widthPixels:int):
+#     center = (int(img.shape[1]/2), int(img.shape[0]/2))
+#     segments = segment_by_deg(img, degInterval, widthPixels, center)
+#     new_pieces = []
+#     for segment in segments:
+#         new_pieces.append(segment[int(center[1] - (widthPixels / 2)):int(center[1] + (widthPixels / 2)),
+#                           int(center[0]):int(center[0] + 999999999)])
+#     return new_pieces
+
+# def vertical_display(segments, cropped:bool, center=None, widthPixels=None):
+#     if cropped:
+#         for s in segments:
+#             plt.figure()
+#             plt.imshow(s)
+#     else:
+#         for segment in segments:
+#             plt.figure()
+#             plt.imshow(segment[int(center[1] - (widthPixels / 2)):int(center[1] + (widthPixels / 2)), int(center[0]):int(center[0] + 999999999)])
+#
+# def horizontal_display(segments, cropped:bool, center=None, widthPixels=None):
+#     # adjust/rotate images
+#     imgs = []
+#     if cropped:
+#         for segment in segments:
+#             imgs.append(rotate_img(segment, 90))
+#     else:
+#         imgs = []
+#         for segment in segments:
+#             new = segment[int(center[1] - (widthPixels / 2)):int(center[1] + (widthPixels / 2)), int(center[0]):int(center[0] + 999999999)]
+#             imgs.append(rotate_img(new, 90))
+#     # build figure and display
+#     plt.figure(figsize=(1, 2 * len(imgs)))
+#     for i in range(len(imgs)):
+#         plt.subplot(1, 2*len(imgs), i + 1)
+#         plt.imshow(imgs[i])
